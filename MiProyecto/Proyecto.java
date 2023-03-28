637f555102680df48db1089013de1eaf1d88ef4e
El-Saka <99428793+El-Saka@users.noreply.github.com>
2023-03-28 19:13:05 -0300

    Add files via upload

---
+++ MiProyecto/Proyecto.java
@@ -0,0 +1,14 @@
+public class Proyecto {
+    public static void main(String[] args){
+        int entero = 10;
+        long largo = 1000000000L;
+        boolean booleano = true;
+        String cadena = "Hola, David!";
+
+        System.out.println("Valor Entero: " + entero);
+        System.out.println("Valor Largo: " + largo);
+        System.out.println("Valor Doble: " + doble);
+        System.out.println("Valor Booleano: " + booleano);
+        System.out.println("Valor Cadena: " + cadena);
+    }
+}
---
+++ MiProyecto/src/Proyecto/Main.java
@@ -0,0 +1,17 @@
+package Proyecto;
+
+public class Main {
+    public static void main(String[] args) {
+        int entero = 10;
+        long largo = 1000000000L;
+        double doble = 3.1416;
+        boolean booleano = true;
+        String cadena = "Hola, mundo!";
+
+        System.out.println("Valor entero: " + entero);
+        System.out.println("Valor largo: " + largo);
+        System.out.println("Valor doble: " + doble);
+        System.out.println("Valor booleano: " + booleano);
+        System.out.println("Valor cadena: " + cadena);
+    }
+}
